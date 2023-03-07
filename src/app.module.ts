90e9d00f1dce74a9f483e379459748b76cf6bab6
FREDVUNI <fredvuni809@gmail.com>
2023-03-07 15:42:29 +0300

    add services and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [AuthModule, UserModule, BookmarkModule],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AppModule {}
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from "@nestjs/common/decorators";
+
+@Controller({})
+export class AuthController{
+
+}
