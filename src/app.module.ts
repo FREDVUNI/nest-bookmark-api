4f197139a5c7f6fcd26ebd1661ddccab01efba6c
FREDVUNI <fredvuni809@gmail.com>
2023-03-07 15:45:01 +0300

    move auth controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [AuthModule, UserModule, BookmarkModule],
-  controllers: [AuthController],
-  providers: [AuthService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 
 @Module({})
 
 export class AuthModule {
-
+    controllers: [AuthController]
+    providers: [AuthService]
 }
