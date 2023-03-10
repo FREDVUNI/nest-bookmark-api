d0a67cdf8e571e9a1b0c7bd244c43ae019a6c2af
FREDVUNI <fredvuni809@gmail.com>
2023-03-10 15:54:30 +0300

    Make email unique in model

--- prisma/migrations/20230310125358_update_models/migration.sql
+++ prisma/migrations/20230310125358_update_models/migration.sql
@@ -1,16 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Bookmark` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "Bookmark";
-
--- DropTable
-DROP TABLE "User";
-
 -- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
@@ -36,6 +23,9 @@ CREATE TABLE "bookmarks" (
 
     CONSTRAINT "bookmarks_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
 ALTER TABLE "bookmarks" ADD CONSTRAINT "bookmarks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
