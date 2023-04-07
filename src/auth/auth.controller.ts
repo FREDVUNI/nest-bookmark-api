052b229fe8627507cbf11daacebee89ca785c326
FREDVUNI <fredvuni809@gmail.com>
2023-04-07 11:18:56 +0300

    run prettier

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,18 @@
-import { Body, Controller, Post } from "@nestjs/common/decorators";
-import { AuthService } from "./auth.service";
-import { AuthDto } from "./dto";
+import { Body, Controller, Post } from '@nestjs/common/decorators';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
-export class AuthController{
-    constructor(private authService:AuthService){}
+export class AuthController {
+  constructor(private authService: AuthService) {}
 
-    @Post('signup')
-    signup(@Body() dto:AuthDto){
-        return this.authService.signup(dto)
-    }
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
 
-    @Post('signin')
-    signin(){
-        return this.authService.login()
-    }
+  @Post('signin')
+  signin() {
+    return this.authService.login();
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,46 +1,41 @@
-import { ForbiddenException, Injectable } from "@nestjs/common";
-import { AuthDto } from "./dto";
-import { PrismaService } from "src/prisma/prisma.service";
-import * as argon from 'argon2'
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { AuthDto } from './dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  login() {}
+  async signup(dto: AuthDto) {
+    //generate hash
+    const hash = await argon.hash(dto.password);
 
-export class AuthService{
-    constructor( private prisma:PrismaService ){}
-    login(){
-        
-    }
-    async signup(dto:AuthDto){
-
-        //generate hash
-        const hash = await argon.hash(dto.password)
-
-        try{
-            //save new users
-            const user = await this.prisma.user.create({
-                data:{
-                    email:dto.email,
-                    hash,
-                },
-                // select:{
-                //     email:true,
-                //     firstName:true,
-                //     lastName:true,
-                //     createdAt:true,
-                //     updatedAt:true
-                // }
-            })
-            delete user.hash;
-            return user;
+    try {
+      //save new users
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        // select:{
+        //     email:true,
+        //     firstName:true,
+        //     lastName:true,
+        //     createdAt:true,
+        //     updatedAt:true
+        // }
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('user with this email already exists.');
         }
-        catch(error){
-            if(error instanceof PrismaClientKnownRequestError){
-                if(error.code === 'P2002'){
-                    throw new ForbiddenException('user with this email already exists.')
-                }
-            }
-            throw error
-        }
+      }
+      throw error;
     }
+  }
 }
