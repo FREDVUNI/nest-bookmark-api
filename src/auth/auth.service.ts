347bace25377bacc9ee0f75d55f92d603ff85a30
FREDVUNI <fredvuni809@gmail.com>
2023-04-08 12:05:21 +0300

    create jwt token for signin and signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { AuthDto } from './dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async signup(dto: AuthDto) {
     //generate hash
     const hash = await argon.hash(dto.password);
@@ -26,8 +32,10 @@ export class AuthService {
         //     updatedAt:true
         // }
       });
-      delete user.hash;
-      return user;
+      // delete user.hash;
+
+      const token = await this.signToken(user.id, user.email);
+      return token;
     } catch (error) {
       // if (error instanceof PrismaClientKnownRequestError) {
       if (error.code === 'P2002') {
@@ -52,7 +60,19 @@ export class AuthService {
     if (!verify_password)
       throw new ForbiddenException('wrong password email combination.');
 
-    delete user.hash;
-    return user;
+    // delete user.hash;
+    const token = await this.signToken(user.id, user.email);
+    return token;
+  }
+
+  signToken(userId: number, email: string): Promise<String> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    return this.jwt.signAsync(payload, {
+      expiresIn: '15min',
+      secret: this.config.get('JWT_SECRET'),
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe(
-    {
-      whitelist:true
-    }
-  ))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(3333);
 }
 bootstrap();
