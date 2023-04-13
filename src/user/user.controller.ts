ccb01bc118c8445e99976741c58437252965cbb1
FREDVUNI <fredvuni809@gmail.com>
2023-04-13 12:22:48 +0300

    create end to auth tests - signin,profile and sign up

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,7 +11,7 @@ export class UserController {
   profile(@GetUser() user: User) {
     return user;
   }
-  @Patch()
+  @Patch('/:userId')
   editUser(@GetUser() userId: number) {
     return userId;
   }
